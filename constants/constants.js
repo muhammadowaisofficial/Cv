af85dcbfcd79c5e509b0ec3afb79f25b0d80c99f
Developer Account <muhammadowaisofficial009@gmail.com>
2024-02-07 04:56:12 +0500

    Cv Updated

---
+++ constants/constants.js
@@ -0,0 +1,75 @@
+export const NAME = "Muhammad Owais";
+
+export const DESIGNATION = `Full Stack Developer | Open Source Contributor | Freelancer`;
+
+export const DETAILS = {
+  Residence: "Pakistan",
+  City: "Karachi",
+  Age: "24",
+};
+
+export const SKILLS = [
+  {
+    title: "Full Stack Developer",
+    level: "75%",
+  },
+  {
+    title: "CMS Developer",
+    level: "91%",
+  },
+  {
+    title: "Wordpress Developer",
+    level: "90%",
+  },
+  {
+    title: "Shopify Developer",
+    level: "74%",
+  },
+  {
+    title: "Front-End Developer",
+    level: "83%",
+  },
+];
+
+export const TECH_STACK = [
+  "Wordpress",
+  "Shopify",
+  "Wix",
+  "SquireSpace",
+  "Headless CMS",
+  "ReactJS",
+  "JavaScript",
+  "NextJS",
+  "TypeScript",
+  "NodeJS",
+  "Redux",
+  "Zustand",
+  "Html 5",
+  "CSS 3",
+  "SCSS",
+  "Bootstrap",
+  "TailwindCSS",
+  "Ant Design",
+  "Material UI",
+  "Next UI",
+  "REST API",
+  "MongoDB",
+  "MySQL",
+  "Git",
+  "GitHub",
+  "Jira",
+  "Figma",
+  "Adobe XD",
+  "Photoshop",
+];
+
+export const CONTACTS = {
+  EMAIL: "muhammadowaisofficial009@gmail.com",
+  PHONE: "+92 3323745184",
+};
+export const SOCIAL_LINKS = {
+  GITHUB: "https://github.com/muhammadowaisofficial",
+  LINKEDIN: "https://www.linkedin.com/in/mohammadowaisofficial",
+  TWITTER: "#",
+  FACEBOOK: "https://www.facebook.com/mohammadowaisofficial1",
+};
